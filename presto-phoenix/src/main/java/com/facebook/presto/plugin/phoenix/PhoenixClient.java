33e8942abde095e8fe0d8fcac49242f2e1d19747
zixian.shen <zixian.shen@gmail.com>
2017-02-08 11:35:08 +0800

    auto refresh phoenix metadata cache

--- presto-phoenix/src/main/java/com/facebook/presto/plugin/phoenix/PhoenixClient.java
+++ presto-phoenix/src/main/java/com/facebook/presto/plugin/phoenix/PhoenixClient.java
@@ -15,14 +15,20 @@ package com.facebook.presto.plugin.phoenix;
 
 import com.facebook.presto.plugin.jdbc.BaseJdbcClient;
 import com.facebook.presto.plugin.jdbc.BaseJdbcConfig;
+import com.facebook.presto.plugin.jdbc.JdbcColumnHandle;
 import com.facebook.presto.plugin.jdbc.JdbcConnectorId;
 import com.facebook.presto.plugin.jdbc.JdbcTableHandle;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.SchemaTableName;
 import com.facebook.presto.spi.type.Type;
+import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import io.airlift.log.Logger;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.phoenix.jdbc.PhoenixDriver;
@@ -30,7 +36,6 @@ import org.apache.phoenix.jdbc.PhoenixDriver;
 import javax.inject.Inject;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -38,6 +43,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.google.common.collect.Iterables.getOnlyElement;
@@ -45,6 +51,9 @@ import static com.google.common.collect.Iterables.getOnlyElement;
 public class PhoenixClient
         extends BaseJdbcClient
 {
+    private static final Logger log = Logger.get(PhoenixClient.class);
+    private static final Integer INF = -1;
+    private LoadingCache<Object, Object> phoenixMetadataCache;
     @Inject
     public PhoenixClient(JdbcConnectorId connectorId, BaseJdbcConfig config)
             throws SQLException
@@ -64,6 +73,11 @@ public class PhoenixClient
                     entry = eItr.next();
                     connectionProperties.put(entry.getKey(), entry.getValue());
                 }
+                int duration = conf.getInt("phoenix.metadata.cache.retention.minutes", 60);
+                // create metadata cache
+                phoenixMetadataCache = CacheBuilder.newBuilder()
+                        .expireAfterAccess(duration, TimeUnit.MINUTES)
+                        .build(createMetadataCacheLoader());
             }
             catch (Exception e) {
                 throw Throwables.propagate(new SQLException("!!!read connection-info!!!", e));
@@ -71,8 +85,44 @@ public class PhoenixClient
         }
     }
 
+    private CacheLoader<Object, Object> createMetadataCacheLoader()
+    {
+        return new CacheLoader<Object, Object>()
+        {
+            @Override
+            public Object load(Object key) throws Exception
+            {
+                long t1 = System.currentTimeMillis();
+                Object ret = null;
+                if (key instanceof Integer) {
+                    ret = makeSchemaNames();
+                }
+                else if (key instanceof String) {
+                    ret = makeTableNames(Strings.emptyToNull((String) key));
+                }
+                else if (key instanceof SchemaTableName) {
+                    ret = makeTableHandle((SchemaTableName) key);
+                }
+                else if (key instanceof JdbcTableHandle) {
+                    ret = makeColumns((JdbcTableHandle) key);
+                }
+                if (ret == null) {
+                    throw Throwables.propagate(new SQLException("!!!Load Phoenix Metadata!!!"));
+                }
+                long t2 = System.currentTimeMillis();
+                log.info("==> GET VAL: %s BY KEY: %s, SPEND %s ms", ret, key, t2 - t1);
+                return ret;
+            }
+        };
+    }
+
     @Override
     public Set<String> getSchemaNames()
+    {
+        return (Set<String>) phoenixMetadataCache.getUnchecked(INF);
+    }
+
+    private Set<String> makeSchemaNames()
     {
         try (Connection connection = driver.connect(connectionUrl, connectionProperties);
              ResultSet resultSet = connection.getMetaData().getSchemas()) {
@@ -100,6 +150,11 @@ public class PhoenixClient
     @Override
     public List<SchemaTableName> getTableNames(String schema)
     {
+        return (List<SchemaTableName>) phoenixMetadataCache.getUnchecked(Strings.nullToEmpty(schema));
+    }
+
+    private List<SchemaTableName> makeTableNames(String schema)
+    {
         try (Connection connection = driver.connect(connectionUrl, connectionProperties)) {
             try (ResultSet resultSet = getTables(connection, schema, null)) {
                 ImmutableList.Builder<SchemaTableName> list = ImmutableList.builder();
@@ -117,8 +172,13 @@ public class PhoenixClient
     @Override
     public JdbcTableHandle getTableHandle(SchemaTableName schemaTableName)
     {
+        return (JdbcTableHandle) phoenixMetadataCache.getUnchecked(schemaTableName);
+    }
+
+    private JdbcTableHandle makeTableHandle(SchemaTableName schemaTableName)
+    {
         try (Connection connection = driver.connect(connectionUrl, connectionProperties)) {
-            DatabaseMetaData metadata = connection.getMetaData();
+            //DatabaseMetaData metadata = connection.getMetaData();
             String jdbcSchemaName = schemaTableName.getSchemaName();
             String jdbcTableName = schemaTableName.getTableName();
 
@@ -138,12 +198,24 @@ public class PhoenixClient
                 if (tableHandles.size() > 1) {
                     throw new PrestoException(NOT_SUPPORTED, "Multiple tables matched: " + schemaTableName);
                 }
-                return getOnlyElement(tableHandles);
+                JdbcTableHandle ret = getOnlyElement(tableHandles);
+                return ret;
             }
         }
         catch (SQLException e) {
             throw Throwables.propagate(e);
         }
+    }
+
+    @Override
+    public List<JdbcColumnHandle> getColumns(JdbcTableHandle tableHandle)
+    {
+        return (List<JdbcColumnHandle>) phoenixMetadataCache.getUnchecked(tableHandle);
+    }
+
+    private List<JdbcColumnHandle> makeColumns(JdbcTableHandle tableHandle)
+    {
+        return super.getColumns(tableHandle);
     }
 
     @Override
