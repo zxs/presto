d33eed7c43b18dadb3c8184392cf80e5e5cde483
zxs <zixian.shen@gmail.com>
2018-03-29 18:59:05 +0800

    release 0.188.100-1

--- presto-assembly/presto-parcel/src/main/resources/assemble.sh
+++ presto-assembly/presto-parcel/src/main/resources/assemble.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 pushd .
 cd ${project.build.directory}
@@ -6,9 +6,12 @@ rm -rf ${parcel.name} repository
 mv ${server.tar.package} ${parcel.name}
 
 #<<COMMENT
-jdk_download_url="http://download.oracle.com/otn-pub/java/jdk/${jdk.version}-${jdk.build}/jdk-${jdk.version}-linux-x64.tar.gz"
-jdk_download_name="jdk.tar.gz"
-# curl -L -o $jdk_download_name -H "Cookie: oraclelicense=accept-securebackup-cookie" $jdk_download_url
+#jdk_download_url="http://download.oracle.com/otn-pub/java/jdk/${jdk.version}-${jdk.build}/jdk-${jdk.version}-linux-x64.tar.gz"
+jdk_download_url="http://127.0.0.1:8000/jdk-${jdk.version}-linux-x64.tar.gz"
+echo $jdk_download_url
+jdk_download_name="jdk-${jdk.version}-linux-x64.tar.gz"
+#curl -L -o $jdk_download_name -H "Cookie: oraclelicense=accept-securebackup-cookie" $jdk_download_url
+wget  $jdk_download_url
 decompressed_dir="extract"
 mkdir $decompressed_dir
 tar xzf $jdk_download_name -C $decompressed_dir
@@ -39,11 +42,11 @@ cp -a ${project.build.outputDirectory}/meta ${parcel.name}
 tar zcf ${parcel.name}.parcel ${parcel.name}/ --owner=root --group=root
 
 mkdir repository
-for i in el5 el6 sles11 lucid precise squeeze wheezy; do
+for i in el5 el6; do
   cp ${parcel.name}.parcel repository/${parcel.name}-${i}.parcel
 done
 
 cd repository
-curl https://raw.githubusercontent.com/cloudera/cm_ext/master/make_manifest/make_manifest.py | python
+curl -s https://raw.githubusercontent.com/cloudera/cm_ext/master/make_manifest/make_manifest.py | python
 
 popd
